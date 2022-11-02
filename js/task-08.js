945687d454c4788b76c992608927a9a80ba6df1d
Irina <iargina.irina@gmail.com>
2022-11-02 23:18:12 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const loginForm = document.querySelector(`.login-form`);
 
+loginForm.addEventListener(`submit`, (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені");
+  }
+  event.currentTarget.reset();
+});
