853e5f82d308ad138bb6e75b2a8507d28071874b
Irina <iargina.irina@gmail.com>
2022-11-02 23:27:57 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const changeButton = document.querySelector(`.change-color`);
+const colorSpan = document.querySelector(`.color`);
+changeButton.addEventListener(`click`, () => {
+  let color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+});
